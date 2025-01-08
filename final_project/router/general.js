26e7800512ee7271388e71a2ccdfbaef10b54677
Mahmood Alhosni <mahmood.a.alhosni@gmail.com>
2025-01-08 21:26:00 +0400

    Completed tasks 10 - 13 and organized code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,118 +3,149 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+// const axios = require('axios').default;
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   // Log the entire req.body object
   // console.log(req.body); 
-  
-   const username = req.body.username;
-   const password = req.body.password;
-   if (username && password) {
-     if (!isValid(username)) {
-       users.push({ "username": username, "password": password });
-       return res.status(200).json({ message: "User successfully registered. Now you can login" });      
-      } else {
-       return res.status(404).json({ message: "User already exists!" });
-     }
-   } else {
-    return res.status(300).json({ message: "username or password is missing"}) 
-   }
-   return res.status(404).json({ message: "Unable to register user." });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+
+  } else {
+    return res.status(500).json({ message: "Unable to register user." });
+  }
+
 });
+
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  // send JSON response with the formatted friends data
-  res.send(JSON.stringify(books, null, 4))
-});
+public_users.get('/', async function (req, res) {
+  try {
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-   // Retrieve the isbn when available for a book
-   const isbnInput = req.params.isbn;
-  //  Need to check if ISPN is part of the parameters in the database and that it is a valid value for any of the value objects.
-   if (books[isbnInput]) {
-    res.send(books[isbnInput]);
-   } else {
-    return res.status(400).json({message: "No ISPN record found on the database"})
-   };
-  
+    res.send(JSON.stringify(books, null, 4)); //await is unecessary here.
 
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const targetAuthor = req.params.author
-  
-  // obtain all the keys for the 'books' object
-  const bookKeys = Object.keys(books);
-  
-  // Loop through the object to find the author's books and return the result.
-  if(targetAuthor) {
-
-    let filteredBooks = bookKeys.filter(key => books[key].author === targetAuthor);
-      if (filteredBooks.length > 0) {
-        // Recreate the new result as an object
-        const returnedObject = filteredBooks.reduce((obj, key) =>{
-          obj[key] = books[key];
-          return obj;
-        }, {});
-
-      res.send(returnedObject)
-        // if 
-      } else {
-
-        return res.status(300).json({message: "No results found"})
-      }
-
-  } else {
-
-  return res.status(400).json({message: "No search query received"});
+  } catch (error) {
+    res.send(500).json({ message: "Unable to fetch book listing.\nError: " + error })
   };
 });
+
+
+// Funcint implementing a Promise to get book details based on ISBN.
+function getBookDetailsFromISBN(isbn) {
+  return new Promise((resolve, reject) => {
+    //  Check if ISPN is part of the parameters in the database and that it is a valid value for any of the value objects.
+    if (books[isbn]) {
+      resolve(books[isbn]);
+    } else {
+      reject("No ISPN record found on the database")
+    };
+  })
+}
+public_users.get('/isbn/:isbn', async function (req, res) {
+
+  try {
+    // Retrieve the isbn when available for a book
+    const isbnInput = req.params.isbn;
+    const bookDetails = await getBookDetailsFromISBN(isbnInput);
+
+    res.send(bookDetails)
+
+  } catch (error) {
+    res.send(500).json({ message: "Unable to find the requested ISBN.\nError: " + error })
+  };
+});
+
+// Get book details based on author
+function getBookDetailsFromAuthor(author) {
+  return new Promise((resolve, reject) => {
+    if (author) {
+      // obtain all the keys for the 'books' object
+      let bookKeys = Object.keys(books);
+      let filteredBooks = bookKeys.filter(key => books[key].author === author);
+      if (filteredBooks.length > 0) {
+        // Recreate the new result as an object
+        const returnedObject = filteredBooks.reduce((obj, key) => {
+          obj[key] = books[key];
+          return (obj);
+        }, {});
+
+        resolve(returnedObject);
+      }
+    } else {
+      reject("Error fetching book details using Author")
+    };
+  })
+};
+
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const targetAuthor = req.params.author
+    // Loop through the object to find the author's books and return the result.
+    const bookDetails = await getBookDetailsFromAuthor(targetAuthor)
+
+    res.send(bookDetails);
+
+  } catch (error) {
+    res.send(500).json({ message: "Unable to fetch results.\nError: " + error })
+  };
+});
+
+
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const targetTitle = req.params.title
-  
-  // obtain all the keys for the 'books' object
-  const bookKeys = Object.keys(books);
-  
-  // Loop through the object to find the author's books and return the result.
-  if(targetTitle) {
-
-    let filteredBooks = bookKeys.filter(key => books[key].title === targetTitle);
+function getBookDetailsFromTitle(title) {
+  return new Promise((resolve, reject) => {
+    if (title) {
+      // obtain all the keys for the 'books' object
+      let bookKeys = Object.keys(books);
+      let filteredBooks = bookKeys.filter(key => books[key].title === title);
       if (filteredBooks.length > 0) {
         // Recreate the new result as an object
-        const returnedObject = filteredBooks.reduce((obj, key) =>{
+        const returnedObject = filteredBooks.reduce((obj, key) => {
           obj[key] = books[key];
-          return obj;
+          return (obj);
         }, {});
 
-      res.send(returnedObject)
-        // if 
-      } else {
-
-        return res.status(300).json({message: "No results found"})
+        resolve(returnedObject);
       }
+    } else {
+      reject("Error fetching book details using Author")
+    };
+  })
+};
 
-  } else {
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    const targetTitle = req.params.title
+    // Loop through the object to find the author's books and return the result.
+    const bookDetails = await getBookDetailsFromTitle(targetTitle)
 
-  return res.status(400).json({message: "No search query received"});
+    res.send(bookDetails)
+
+  } catch (error) {
+    res.send(500).json({ message: "Unable to fetch results.\nError: " + error })
   };
+
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
- // Retrieve the isbn when available for a book
- const isbnInput = req.params.isbn;
- //  Need to check if ISPN is part of the parameters in the database and that it is a valid value for any of the value objects.
+public_users.get('/review/:isbn', function (req, res) {
+  // Retrieve the isbn when available for a book
+  const isbnInput = req.params.isbn;
+  //  Need to check if ISPN is part of the parameters in the database and that it is a valid value for any of the value objects.
   if (books[isbnInput]) {
-   res.send(books[isbnInput].reviews);
+    res.send(books[isbnInput].reviews);
   } else {
-   return res.status(400).json({message: "No ISPN record found on the database"})
+    return res.status(400).json({ message: "No ISPN record found on the database" })
   };
 });
 
